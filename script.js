82f8317ebe1a7fc2bba548dac89c86a97e6ed756
Anapy34 <156463751+Anapy34@users.noreply.github.com>
2024-01-27 20:55:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const albums = [
-        {
-            name: 'Album 1',
-            cover: 'https://img.youtube.com/vi/QnCs2nsZGRk/mqdefault.jpg',
-            videos: [
-                'https://www.youtube.com/embed/QnCs2nsZGRk',
-                'https://www.youtube.com/embed/gMcbiuNYnQY'
-            ]
-        },
-        {
-            name: 'Album 2',
-            cover: '',
-            videos: [
-              'https://player.vimeo.com/video/384878999',
-               'https://player.vimeo.com/video/307791576',
-              'https://player.vimeo.com/video/378788675',
-		'https://vimeo.com/345036555',
-              'https://vimeo.com/306676681',
-                
-            ]
-        },
-        {
-            name: 'Album 3',
-            cover: 'https://i3.ytimg.com/vi/v3SQTOZO36E/hqdefault.jpg',
-            videos: [
-                     'https://www.youtube.com/embed/v3SQTOZO36E',
-                     'https://www.youtube.com/embed/Zdh2hot8rjU',
-                     'https://www.youtube.com/embed/GXk9e1K52L4'
-                    ]
-        },
-       {
-            name: 'Album 4',
-            cover: 'https://cdn-icons-png.flaticon.com/512/7405/7405885.png',
-        videos: [
-                 'https://www.xvideos.com/embedframe/74149425',
-                 'https://www.pornhub.com/embed/ph63286e3a24438',
-                 'https://br.xhamster.com/embed/xhLcoji',
-		 'https://br.xhamster.com/embed/xhpVPMp',
-           
-		   ]     
-        },
-    ];
-
+	
     let currentAlbumIndex = 0;
     let currentVideoIndex = 0;
+
+    // Use albums como uma variável global
+    const albums = window.albums;
+
 
     function showVideo(albumIndex, videoIndex) {
         const videoSrc = albums[albumIndex].videos[videoIndex];

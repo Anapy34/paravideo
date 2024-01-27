17094f7e368e1ffd152deb908124d58823f15091
Anapy34 <156463751+Anapy34@users.noreply.github.com>
2024-01-27 13:01:11 -0300

    02
    02

---
+++ PlayVideo/pvideo.js
@@ -0,0 +1,17 @@
+const albums = [
+        {
+            name: 'Album 1',
+            cover: 'https://img.youtube.com/vi/QnCs2nsZGRk/mqdefault.jpg',
+            videos: ['https://www.youtube.com/embed/QnCs2nsZGRk', 'https://www.youtube.com/embed/gMcbiuNYnQY']
+        },
+        {
+            name: 'Album 2',
+            cover: 'https://i.vimeocdn.com/video/747831242-883562d229153aaf8eabe591d991419b6af862c5105fd83ab44329403214fd96-d?mw=700&mh=394',
+            videos: ['https://player.vimeo.com/video/307791576', 'https://player.vimeo.com/video/384878999']
+        },
+        {
+            name: 'Album 3',
+            cover: 'https://i3.ytimg.com/vi/v3SQTOZO36E/hqdefault.jpg',
+            videos: ['https://www.youtube.com/embed/v3SQTOZO36E', 'https://www.youtube.com/embed/Zdh2hot8rjU', 'https://www.youtube.com/embed/GXk9e1K52L4']
+        },
+];
